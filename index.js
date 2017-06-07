e066c50297b78f6614538d6125ae855a24bdcd03
simaQ <sima.zhang1990@gmail.com>
2017-06-07 11:50:42 +0800

    Canvas.Util 更新

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ Canvas.G = G
 Canvas.Group = G.Group
 Canvas.Shape = {}
 Canvas.Shape.Marker = G.Marker
-Canvas.Util = require('./src/util/util')
+Canvas.Util = require('./src/util/index')
 Canvas.Matrix = require('@ali/g-matrix')
 module.exports = Canvas
