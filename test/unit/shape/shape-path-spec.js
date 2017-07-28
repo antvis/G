6c85de4fac6405d537f53c5da35241be59c7e4ce
leungwensen <leungwensen@gmail.com>
2017-07-28 11:09:05 +0800

    testing structure

--- test/unit/shape/shape-path-spec.js
+++ test/unit/shape/shape-path-spec.js
@@ -1,4 +1,7 @@
 const expect = require('chai').expect;
+const {
+  resolve
+} = require('path');
 const G = require('../../../index');
 const Canvas = require('../../../src/canvas');
 const div = document.createElement('div');
@@ -15,7 +18,7 @@ describe('Path', function() {
   });
 
   const img = document.createElement('img');
-  img.src = '../../../demos/test1.jpg';
+  img.src = resolve(process.cwd(), './test/fixtures/test1.jpg');
   img.id = 'img';
   document.body.appendChild(img);
 
