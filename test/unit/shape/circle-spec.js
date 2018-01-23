0f4abc417d7cbaab1bb285e29c9d868a64764054
有田 <huangtong.ht@alipay.com>
2018-01-23 18:05:02 +0800

    add repeat

--- test/unit/shape/circle-spec.js
+++ test/unit/shape/circle-spec.js
@@ -1,10 +1,11 @@
 const expect = require('chai').expect;
 const G = require('../../../index');
 const Canvas = require('../../../src/canvas');
-const div = document.createElement('div');
-div.id = 'canvas-circle';
-document.body.appendChild(div);
+
 describe('Circle', function() {
+  const div = document.createElement('div');
+  div.id = 'canvas-circle';
+  document.body.appendChild(div);
   const canvas = new Canvas({
     containerId: 'canvas-circle',
     width: 200,
--- test/unit/shape/text-spec.js
+++ test/unit/shape/text-spec.js
@@ -1,12 +1,11 @@
 const expect = require('chai').expect;
 const G = require('../../../index');
 const Canvas = require('../../../src/canvas');
-const div = document.createElement('div');
-div.id = 'canvas-text';
-document.body.appendChild(div);
-
 
 describe('Text', function() {
+  const div = document.createElement('div');
+  div.id = 'canvas-text';
+  document.body.appendChild(div);
   const canvas = new Canvas({
     containerId: 'canvas-text',
     width: 200,
@@ -422,7 +421,9 @@ describe('Text', function() {
 });
 
 describe('Text \n', function() {
-
+  const div = document.createElement('div');
+  div.id = 'canvas-text';
+  document.body.appendChild(div);
   const canvas = new Canvas({
     containerId: 'canvas-text',
     width: 200,
@@ -472,7 +473,9 @@ describe('Text \n', function() {
 });
 
 describe('Text 不存在', function() {
-
+  const div = document.createElement('div');
+  div.id = 'canvas-text';
+  document.body.appendChild(div);
   const canvas = new Canvas({
     containerId: 'canvas-text',
     width: 200,
