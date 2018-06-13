80ea1ef9cfca7c2c84953833547d61dcc1c9febd
Elaine1234 <yilin.qyl@antfin.com>
2018-06-13 10:20:03 +0800

    fix: setZIndex is a public method, remove _

--- src/svg/core/element.js
+++ src/svg/core/element.js
@@ -161,7 +161,7 @@ Util.augment(Element, Attribute, Transform, EventEmitter, Animate, {
     this.attr(attrs);
     return attrs;
   },
-  _setZIndex(zIndex) {
+  setZIndex(zIndex) {
     this.__cfg.zIndex = zIndex;
     return zIndex;
   },
