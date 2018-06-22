6967bde679d1f767594e60a846f7f795c544d877
leungwensen <leungwensen@gmail.com>
2018-06-22 17:30:40 +0800

    chore(dev): G.version and related test case

--- test/unit/index-spec.js
+++ test/unit/index-spec.js
@@ -1,9 +1,10 @@
 const expect = require('chai').expect;
-const G = require('../../../src/svg/index');
+const G = require('../../src/index');
+const pkg = require('../../package.json');
 
 describe('index', () => {
   it('G', () => {
     expect('G').to.be.a('string');
-    expect(G).to.be.a('object');
+    expect(G.version).to.equal(pkg.version);
   });
 });
