db50e55c3659634c502d594685e93bf7fb120587
wadezhan <wadezhan@tencent.com>
2018-07-03 10:53:24 +0800

    remove duplicate code

--- src/canvas/shape/index.js
+++ src/canvas/shape/index.js
@@ -1,6 +1,5 @@
 const Shape = require('../core/shape');
 Shape.Rect = require('./rect');
-Shape.Rect = require('./rect');
 Shape.Circle = require('./circle');
 Shape.Ellipse = require('./ellipse');
 Shape.Path = require('./path');
