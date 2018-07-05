3a9594b1d7740a2a50aa262cec05f0553655f631
Elaine1234 <yilin.qyl@antfin.com>
2018-07-05 18:01:48 +0800

    refactor: add abstract interfaces

--- src/core/mixin/animation.js
+++ src/core/mixin/animation.js
@@ -1,5 +1,5 @@
-const Util = require('../index');
-const MatrixUtil = require('../matrix');
+const Util = require('../../util/index');
+const MatrixUtil = require('../../util/matrix');
 
 const ReservedProps = { delay: 'delay' };
 
