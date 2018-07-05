f1d06dfe93f61b798468b64e0fb24ad7d24fa41d
Elaine1234 <yilin.qyl@antfin.com>
2018-07-05 18:07:43 +0800

    refactor: add interfaces of shapes

---
+++ src/shapes/index.js
@@ -0,0 +1,18 @@
+const Shape = require('../core/shape');
+Shape.Rect = require('./rect');
+Shape.Rect = require('./rect');
+Shape.Circle = require('./circle');
+Shape.Ellipse = require('./ellipse');
+Shape.Path = require('./path');
+Shape.Text = require('./text');
+Shape.Line = require('./line');
+Shape.Image = require('./image');
+Shape.Polygon = require('./polygon');
+Shape.Polyline = require('./polyline');
+Shape.Arc = require('./arc');
+Shape.Fan = require('./fan');
+Shape.Cubic = require('./cubic');
+Shape.Quadratic = require('./quadratic');
+Shape.Marker = require('./marker');
+
+module.exports = Shape;
