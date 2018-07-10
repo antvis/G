734288f623dc94305913b203698ce61c9ce37a98
Elaine1234 <yilin.qyl@antfin.com>
2018-07-10 17:14:24 +0800

    refactor: add dependency of svg renderer

---
+++ src/renderers/svg/defs/shadow.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Elaine on 2018/5/10.
+ */
+const Util = require('../../../util/index');
+
+const ATTR_MAP = {
+  shadowColor: 'color',
+  shadowOpacity: 'opacity',
+  shadowBlur: 'blur',
+  shadowOffsetX: 'dx',
+  shadowOffsetY: 'dy'
+};
+
+class Shadow {
+  constructor(cfg) {
+    this.type = 'shadow';
+    const el = document.createElementNS('http://www.w3.org/2000/svg', 'filter');
+    this.el = el;
+    this.id = Util.uniqueId('filter_');
+    this.el.id = this.id;
+    this.cfg = cfg;
+    this._parseShadow(cfg, el);
+    return this;
+  }
+  match(type, cfg) {
+    if (this.type !== type) {
+      return false;
+    }
+    let flag = false;
+    const config = this.__attrs.config;
+    Util.each(Object.keys(config), attr => {
+      if (!flag) {
+        flag = config[attr] === cfg[attr];
+      }
+    });
+    return flag;
+  }
+  update(name, value) {
+    const config = this.cfg;
+    config[ATTR_MAP[name]] = value;
+    this._parseShadow(config, this.el);
+    return this;
+  }
+  _parseShadow(config, el) {
+    const child = `<feDropShadow 
+      dx="${config.dx}" 
+      dy="${config.dy}" 
+      stdDeviation="${config.blur ? config.blur / 10 : 0}"
+      flood-color="${config.color ? config.color : '#000'}"
+      flood-opacity="${config.opacity ? config.opacity : 1}"
+      />`;
+    el.innerHTML = child;
+  }
+}
+
+module.exports = Shadow;
+
