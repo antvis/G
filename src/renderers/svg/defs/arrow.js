7d44261d525fa67223a5323ad1c61a6055477367
Elaine1234 <yilin.qyl@antfin.com>
2018-07-11 18:22:27 +0800

    fix: fix `marker` type svg arrow bug

--- src/renderers/svg/defs/arrow.js
+++ src/renderers/svg/defs/arrow.js
@@ -3,60 +3,65 @@
  */
 const Util = require('../../../util/index');
 
-const DEFAULT_PATH = {
-  'marker-start': 'M6,0 L0,3 L6,6 L3,3Z',
-  'marker-end': 'M0,0 L6,3 L0,6 L3,3Z'
-};
-
 class Arrow {
   constructor(attrs, type) {
     const el = document.createElementNS('http://www.w3.org/2000/svg', 'marker');
     const id = Util.uniqueId('marker_');
     el.setAttribute('id', id);
+    const shape = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    shape.setAttribute('stroke', 'none');
+    shape.setAttribute('fill', attrs.stroke || '#000');
+    el.appendChild(shape);
+    el.setAttribute('markerWidth', 16);
+    el.setAttribute('markerHeight', 16);
     this.el = el;
+    this.child = shape;
     this.id = id;
     this.cfg = attrs[type === 'marker-start' ? 'startArrow' : 'endArrow'];
     this.stroke = attrs.stroke || '#000';
     if (this.cfg === true) {
-      this._setDefaultPath(type);
+      this._setDefaultPath(type, shape);
     } else {
-      this._setMarker(attrs[type]);
+      this._setMarker(attrs.lineWidth, shape);
     }
     return this;
   }
   match() {
     return false;
   }
-  _setDefaultPath(type) {
+  _setDefaultPath(type, el) {
     const parent = this.el;
-    const el = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-    el.setAttribute('d', DEFAULT_PATH[type]);
-    el.setAttribute('stroke', 'none');
-    el.setAttribute('fill', this.stroke || '#000');
-    this.child = el;
-    parent.appendChild(el);
+    el.setAttribute('d', 'M0,0 L6,3 L0,6 L3,3Z');
     parent.setAttribute('refX', 3);
     parent.setAttribute('refY', 3);
-    parent.setAttribute('markerWidth', 16);
-    parent.setAttribute('markerHeight', 16);
-    parent.setAttribute('orient', 'auto');
+    parent.setAttribute('orient', 'auto-start-reverse');
   }
-  _setMarker() {
+  _setMarker(r, el) {
     const parent = this.el;
     const shape = this.cfg;
-    const el = shape._cfg.el;
+    const attrs = shape._attrs;
     if (shape.type !== 'marker') {
       throw new TypeError('the shape of an arrow should be an instance of Marker');
     }
-    el.setAttribute('stroke', 'none');
-    el.setAttribute('fill', this.stroke);
-    parent.append(el);
-    const width = shape._attrs.x;
-    const height = shape._attrs.y;
-    parent.setAttribute('refX', width);
-    parent.setAttribute('refY', height);
-    parent.setAttribute('markerWidth', width * 2);
-    parent.setAttribute('markerHeight', height * 2);
+    if (!attrs.x) {
+      attrs.x = r;
+    }
+    if (!attrs.y) {
+      attrs.y = r;
+    }
+    if (!attrs.r && !attrs.radius) {
+      attrs.r = r;
+    }
+    let path = shape._getPath();
+    if (Util.isArray(path)) {
+      path = path.map(segment => {
+        return segment.join(' ');
+      }).join('');
+    }
+    el.setAttribute('d', path);
+    parent.appendChild(el);
+    parent.setAttribute('refX', attrs.x);
+    parent.setAttribute('refY', attrs.y);
     parent.setAttribute('orient', 'auto');
   }
   update(fill) {
