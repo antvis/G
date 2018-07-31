a0168ab900d60e131cd8892d8b8f9f4a77906fb7
yilin.qyl <yilin.qyl@alipay.com>
2018-07-31 17:48:24 +0800

    fix: no need to stop shape's timer now

--- src/core/element.js
+++ src/core/element.js
@@ -128,11 +128,6 @@ Util.augment(Element, Attribute, Transform, EventEmitter, Animate, {
     if (destroyed) {
       return;
     }
-    // 如果正在执行动画，清理动画
-    if (this.get('animating')) {
-      const timer = this.get('animateTimer');
-      timer && timer.stop();
-    }
     this._attrs = null;
     this.removeEvent(); // 移除所有的事件
     this._cfg = {
