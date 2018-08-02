56ca14f4dee3b2c7121bdeac8ac24dd4c6fc4c75
Elaine1234 <yilin.qyl@antfin.com>
2018-08-02 14:42:08 +0800

    fix: fix event of shape `dom`

--- src/renderers/svg/getShape.js
+++ src/renderers/svg/getShape.js
@@ -1,10 +1,29 @@
+const TAG_MAP = {
+  svg: 'svg',
+  circle: 'circle',
+  rect: 'rect',
+  text: 'text',
+  path: 'path',
+  foreignObject: 'foreignObject',
+  polygon: 'polygon',
+  ellipse: 'ellipse',
+  image: 'image'
+};
+
 module.exports = function getShape(x, y, e) {
   const target = e.target || e.srcElement;
-  const id = target.id;
+  let id = target.id;
+  if (!id || !TAG_MAP[target.tagName]) {
+    let parent = target.parentNode;
+    while (parent && !TAG_MAP[parent.tagName]) {
+      parent = parent.parentNode;
+    }
+    id = parent.id;
+  }
   if (this._attrs.id === id) {
     return this;
   }
   return this.find(item => {
-    return item.attrs && item._attrs.id === id;
+    return item._attrs && item._attrs.id === id;
   });
 };
