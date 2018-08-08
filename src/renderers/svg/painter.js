3249b46c384a0b0e4b7bc28a186ccc86b98a66b1
Elaine1234 <yilin.qyl@antfin.com>
2018-08-08 18:22:30 +0800

    feat: add support for drawing radius array of rect in svg

--- src/renderers/svg/painter.js
+++ src/renderers/svg/painter.js
@@ -3,7 +3,7 @@ const Marker = require('../../shapes/marker');
 const Defs = require('./defs');
 
 const SHAPE_TO_TAGS = {
-  rect: 'rect',
+  rect: 'path',
   circle: 'circle',
   line: 'line',
   path: 'path',
@@ -145,6 +145,13 @@ class Painter {
     const attrs = model._attrs;
     const cfg = model._cfg;
     let el = cfg.el;
+    // 删除
+    if (cfg.removed || cfg.destroyed) {
+      if (el) {
+        el.parentNode.removeChild(cfg.el);
+      }
+      return;
+    }
 
     // 重绘节点
     if (redraw && el) {
@@ -172,6 +179,7 @@ class Painter {
     if (cfg.hasUpdate) {
       self._updateShape(model);
     }
+
     if (attrs.clip && attrs.clip._cfg.hasUpdate) {
       self._updateShape(attrs.clip);
     }
@@ -202,6 +210,9 @@ class Painter {
     if (model.type === 'marker') {
       model._cfg.el.setAttribute('d', self._assembleMarker(attrs));
     }
+    if (model.type === 'rect') {
+      model._cfg.el.setAttribute('d', self._assembleRect(attrs));
+    }
     for (const key in attrs) {
       if (attrs[key] !== formerAttrs[key]) {
         self._setAttribute(model, key, attrs[key]);
@@ -217,18 +228,12 @@ class Painter {
     const defs = this.context;
 
     // 计算marker路径
-    if (type === 'marker' && ~[ 'x', 'y', 'radius', 'r' ].indexOf(name)) {
+    if ((type === 'marker' || type === 'rect') && ~[ 'x', 'y', 'radius', 'r' ].indexOf(name)) {
       return;
     }
     // 圆和椭圆不是x, y， 是cx, cy。 marker的x,y 用于计算marker的路径，不需要写到dom
     if (~[ 'circle', 'ellipse' ].indexOf(type) && ~[ 'x', 'y' ].indexOf(name)) {
       el.setAttribute('c' + name, parseInt(value, 10));
-      return;
-    }
-    // 圆角矩形
-    if (type === 'rect' && name === 'radius') {
-      el.setAttribute('rx', value);
-      el.setAttribute('ry', value);
       return;
     }
     // 多边形
@@ -357,6 +362,53 @@ class Painter {
       }).join('');
     }
     return d;
+  }
+  _assembleRect(attrs) {
+    const x = attrs.x;
+    const y = attrs.y;
+    const w = attrs.width;
+    const h = attrs.height;
+    const radius = attrs.radius;
+
+    if (!radius) {
+      return `M ${x},${y} l ${w},0 l 0,${h} l${-w} 0 z`;
+    }
+    let r1 = 0;
+    let r2 = 0;
+    let r3 = 0;
+    let r4 = 0;
+    if (Util.isArray(radius)) {
+      if (radius.length === 1) {
+        r1 = r2 = r3 = r4 = radius[0];
+      } else if (radius.length === 2) {
+        r1 = r3 = radius[0];
+        r2 = r4 = radius[1];
+      } else if (radius.length === 3) {
+        r1 = radius[0];
+        r2 = r4 = radius[1];
+        r3 = radius[2];
+      } else {
+        r1 = radius[0];
+        r2 = radius[1];
+        r3 = radius[2];
+        r4 = radius[3];
+      }
+    } else {
+      r1 = r2 = r3 = r4 = radius;
+    }
+    const d = [
+      [ `M ${x + r1},${y}` ],
+      [ `l ${w - r1 - r2},0` ],
+      [ `a ${r2},${r2},0,0,1,${r2},${r2}` ],
+      [ `l 0,${h - r2 - r3}` ],
+      [ `a ${r3},${r3},0,0,1,${-r3},${r3}` ],
+      [ `l ${r3 + r4 - w},0` ],
+      [ `a ${r4},${r4},0,0,1,${-r4},${-r4}` ],
+      [ `l 0,${r4 + r1 - h}` ],
+      [ `a ${r1},${r1},0,0,1,${r1},${-r1}` ],
+      [ 'z' ]
+    ];
+    return d.join(' ');
   }
   _formatPath(value) {
     value = value.map(path => {
