e33f955a89ab4fddbe4c9c2e4613410421f4778f
Elaine1234 <yilin.qyl@antfin.com>
2018-08-14 13:51:03 +0800

    feat: add path handler `fillPathByDiff`

--- src/util/path.js
+++ src/util/path.js
@@ -1081,6 +1081,109 @@ const fillPath = function(source, target) {
   return filled;
 };
 
+const isEqual = function(obj1, obj2) {
+  if (obj1.length !== obj2.length) {
+    return false;
+  }
+  let result = true;
+  Util.each(obj1, (item, i) => {
+    if (item !== obj2[i]) {
+      result = false;
+      return false;
+    }
+  });
+  return result;
+};
+function getMinDiff(del, add, modify) {
+  let type = 'del';
+  let min = del;
+  if (add < min) {
+    min = add;
+    type = 'add';
+  }
+  if (modify < min) {
+    min = modify;
+    type = null;
+  }
+  return {
+    type,
+    min
+  };
+}
+
+const levenshteinDistance = function(source, target) {
+  const sourceLen = source.length;
+  const targetLen = target.length;
+  let sourceSegment,
+    targetSegment;
+  let temp = 0;
+  if (sourceLen === 0 || targetLen === 0) {
+    return null;
+  }
+  const dist = new Array(sourceLen + 1);
+  for (let i = 0; i <= sourceLen; i++) {
+    dist[i] = new Array(targetLen + 1);
+    dist[i][0] = { min: i };
+  }
+  for (let j = 0; j <= targetLen; j++) {
+    dist[0][j] = { min: j };
+  }
+
+  for (let i = 1; i <= sourceLen; i++) {
+    sourceSegment = source[i - 1];
+    for (let j = 1; j <= targetLen; j++) {
+      targetSegment = target[j - 1];
+      if (isEqual(sourceSegment, targetSegment)) {
+        temp = 0;
+      } else {
+        temp = 1;
+      }
+      const del = dist[i - 1][j].min + 1;
+      const add = dist[i][j - 1].min + 1;
+      const modify = dist[i - 1][j - 1].min + temp;
+      dist[i][j] = getMinDiff(del, add, modify);
+    }
+  }
+  return dist;
+};
+
+const fillPathByDiff = function(source, target) {
+  const diffMatrix = levenshteinDistance(source, target);
+  const sourceLen = source.length;
+  const targetLen = target.length;
+  const changes = [];
+  let index = 1;
+  for (let i = 1; i <= sourceLen; i++) {
+    let min = diffMatrix[i][index].min;
+    for (let j = index; j <= targetLen; j++) {
+      if (diffMatrix[i][j].min < min) {
+        min = diffMatrix[i][j].min;
+        index = j;
+      }
+    }
+    if (diffMatrix[i][index].type) {
+      changes.push({ index: i - 1, type: diffMatrix[i][index].type });
+    }
+  }
+
+  if (index < targetLen) {
+    for (let i = index + 1; i <= targetLen; i++) {
+      source.push(source[sourceLen - 1]);
+    }
+  }
+
+  for (let i = changes.length - 1; i >= 0; i--) {
+    index = changes[i].index;
+    if (changes[i].type === 'add') {
+      source.splice(index, 0, [].concat(source[index]));
+    } else {
+      source.splice(index, 1);
+    }
+  }
+  console.log(source);
+  return source;
+};
+
 module.exports = {
   parsePathString,
   parsePathArray,
@@ -1089,5 +1192,6 @@ module.exports = {
   catmullRomToBezier: catmullRom2bezier,
   rectPath,
   fillPath,
+  fillPathByDiff,
   intersection: pathIntersection
 };
