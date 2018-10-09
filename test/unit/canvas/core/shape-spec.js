bc6a2587e12c984b39129fead4a6d28ebd508bc4
yilin.qyl <yilin.qyl@alipay.com>
2018-10-09 20:44:39 +0800

    feat(shape): add unit test of clone for shape

--- test/unit/canvas/core/shape-spec.js
+++ test/unit/canvas/core/shape-spec.js
@@ -1,8 +1,9 @@
 const expect = require('chai').expect;
 const Shape = require('../../../../src/core/shape');
+const Group = require('../../../../src/core/group');
 
 describe('Shape', function() {
-  it('clone shape', () => {
+  it('clone path', () => {
     const path = new Shape.Path({
       attrs: {
         path: [
@@ -27,5 +28,24 @@ describe('Shape', function() {
     path._attrs.path[0][1] = 10;
     expect(path._attrs.path[0][1]).to.equal(10);
     expect(clone._attrs.path[0][1]).to.equal(5);
+  });
+  it('clone shape with parent', () => {
+    const group = new Group();
+    const polygon = group.addShape('polygon', {
+      attrs: {
+        points: [
+          [ 10, 10 ], [ 20, 20 ], [ 50, 50 ]
+        ]
+      }
+    });
+    expect(polygon._cfg.parent).not.to.be.undefined;
+    const clone = polygon.clone();
+    expect(polygon._cfg.parent).not.to.be.undefined;
+    expect(clone._cfg.parent).to.be.undefined;
+    expect(polygon._attrs.points[0][0]).to.equal(10);
+    expect(clone._attrs.points[0][0]).to.equal(10);
+    polygon._attrs.points[0][0] = 20;
+    expect(polygon._attrs.points[0][0]).to.equal(20);
+    expect(clone._attrs.points[0][0]).to.equal(10);
   });
 });
