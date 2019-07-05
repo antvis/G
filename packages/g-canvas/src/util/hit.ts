cf9b233cf8962951ba4505adaa7a7910e0cb56c0
萧庆 <xiaoqing.dongxq@antfin.com>
2019-07-05 18:05:06 +0800

    feat(shape): add ellipse

--- packages/g-canvas/src/util/hit.ts
+++ packages/g-canvas/src/util/hit.ts
@@ -5,6 +5,11 @@
 
 import InStroke from './in-stroke';
 import { inBox, distance } from './util';
+
+// 根据椭圆公式计算 x*x/rx*rx + y*y/ry*ry;
+function ellipseDistance(xSquare, ySquare, rx, ry) {
+  return xSquare / (rx * rx) + ySquare / (ry * ry);
+}
 // 全局设置一个唯一离屏的 ctx，用于计算 isPointInPath
 let offScreenCtx = null;
 function getOffScreenContext() {
@@ -16,6 +21,24 @@ function getOffScreenContext() {
   }
   return offScreenCtx;
 }
+
+// 多边形的射线检测
+// function isInPolygon(points, x, y) {
+//   let crossings = 0;
+//   for (let i = 0; i < points.length; i++) {
+//     const point = points[i];
+//     const px = point[0];
+//     const py = point[1];
+//     const nextPoint = i == points.length - 1 ? points[0] : points[i + 1];
+//     const slope = (nextPoint[1] - py) / (nextPoint[0] - px);
+//     const cond = (px <= x) && (x < nextPoint[0]) || (py <= y) && (y < nextPoint[1]);
+//     const above = y < slope * (x - px) + py;
+//     if (cond && above) {
+//       crossings++
+//     }
+//   }
+//   return crossings % 2 !== 0;
+// }
 
 function isPointInPath(shape, x, y) {
   const ctx = getOffScreenContext();
@@ -52,7 +75,7 @@ const HitUtil = {
       isHit = InStroke.polygon(points, lineWidth, x, y);
     }
     if (!isHit && isFill) {
-      isHit = isPointInPath(shape, x, y);
+      isHit = isPointInPath(shape, x, y); // isInPolygon(points, x, y); //
     }
     return isHit;
   },
@@ -64,14 +87,41 @@ const HitUtil = {
     const cy = attrs.y;
     const r = attrs.r;
     const lineWidth = shape.getHitLineWidth();
+    const halfLineWidth = lineWidth / 2;
+    const absDistance = distance(cx, cy, x, y);
+    // 直接用距离，如果同时存在边和填充时，可以减少两次计算
     if (isFill && isStroke) {
-      return distance(cx, cy, x, y) <= r + lineWidth / 2;
+      return absDistance <= r + halfLineWidth;
     }
     if (isFill) {
-      return distance(cx, cy, x, y) <= r;
+      return absDistance <= r;
     }
-
     if (isStroke) {
+      return absDistance >= r - halfLineWidth && absDistance <= r + halfLineWidth;
+    }
+    return false;
+  },
+  ellipse(shape, x, y) {
+    const isFill = shape.isFill();
+    const isStroke = shape.isStroke();
+    const attrs = shape.attr();
+    const lineWidth = shape.getHitLineWidth();
+    const halfLineWith = lineWidth / 2;
+    const cx = attrs.x;
+    const cy = attrs.y;
+    const {rx, ry} = attrs;
+    const tmpX = (x - cx) * (x - cx);
+    const tmpY = (y - cy) * (y - cy);
+    // 使用椭圆的公式： x*x/rx*rx + y*y/ry*ry = 1; 
+    if (isFill && isStroke) {
+      return ellipseDistance(tmpX, tmpY, rx + halfLineWith, ry + halfLineWith) <= 1;
+    }
+    if (isFill) {
+      return ellipseDistance(tmpX, tmpY, rx, ry) <= 1;
+    }
+    if (isStroke) {
+      return ellipseDistance(tmpX, tmpY, rx - halfLineWith, ry - halfLineWith) >= 1 && 
+        ellipseDistance(tmpX, tmpY, rx + halfLineWith, ry + halfLineWith) <= 1;
     }
     return false;
   },
@@ -104,6 +154,7 @@ const HitUtil = {
       if (isStroke) {
         isHit = InStroke.rectWithRadius(minX, minY, width, height, radius, lineWidth, x, y);
       }
+      // 仅填充时带有圆角的矩形直接通过图形拾取
       if (!isHit && isFill) {
         isHit = isPointInPath(shape, x, y);
       }
---
+++ packages/g-canvas/src/util/math/ellipse.ts
@@ -0,0 +1,98 @@
+
+import { mod, toRadian} from '../util';
+
+// 向量长度
+function vMag(v) {
+  return Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+}
+
+// u.v/|u||v|
+function vRatio(u, v) {
+  return (u[0] * v[0] + u[1] * v[1]) / (vMag(u) * vMag(v));
+}
+
+// 向量角度
+function vAngle(u, v) {
+  return (u[0] * v[1] < u[1] * v[0] ? -1 : 1) * Math.acos(vRatio(u, v));
+}
+
+// A 0:rx 1:ry 2:x-axis-rotation 3:large-arc-flag 4:sweep-flag 5: x 6: y
+function getArcParams(startPoint, params) {
+  let rx = params[1];
+  let ry = params[2];
+  const xRotation = mod(toRadian(params[3]), Math.PI * 2);
+  const arcFlag = params[4];
+  const sweepFlag = params[5];
+  const x1 = startPoint[0];
+  const y1 = startPoint[1];
+  const x2 = params[6];
+  const y2 = params[7];
+  const xp = Math.cos(xRotation) * (x1 - x2) / 2.0 + Math.sin(xRotation) * (y1 - y2) / 2.0;
+  const yp = -1 * Math.sin(xRotation) * (x1 - x2) / 2.0 + Math.cos(xRotation) * (y1 - y2) / 2.0;
+  const lambda = (xp * xp) / (rx * rx) + (yp * yp) / (ry * ry);
+
+  if (lambda > 1) {
+    rx *= Math.sqrt(lambda);
+    ry *= Math.sqrt(lambda);
+  }
+  const diff = (rx * rx) * (yp * yp) + (ry * ry) * (xp * xp);
+  let f = Math.sqrt((((rx * rx) * (ry * ry)) - diff) / diff);
+
+  if (arcFlag === sweepFlag) {
+    f *= -1;
+  }
+  if (isNaN(f)) {
+    f = 0;
+  }
+
+  const cxp = f * rx * yp / ry;
+  const cyp = f * -ry * xp / rx;
+
+  const cx = (x1 + x2) / 2.0 + Math.cos(xRotation) * cxp - Math.sin(xRotation) * cyp;
+  const cy = (y1 + y2) / 2.0 + Math.sin(xRotation) * cxp + Math.cos(xRotation) * cyp;
+
+  const theta = vAngle([ 1, 0 ], [ (xp - cxp) / rx, (yp - cyp) / ry ]);
+  const u = [ (xp - cxp) / rx, (yp - cyp) / ry ];
+  const v = [ (-1 * xp - cxp) / rx, (-1 * yp - cyp) / ry ];
+  let dTheta = vAngle(u, v);
+
+  if (vRatio(u, v) <= -1) {
+    dTheta = Math.PI;
+  }
+  if (vRatio(u, v) >= 1) {
+    dTheta = 0;
+  }
+  if (sweepFlag === 0 && dTheta > 0) {
+    dTheta = dTheta - 2 * Math.PI;
+  }
+  if (sweepFlag === 1 && dTheta < 0) {
+    dTheta = dTheta + 2 * Math.PI;
+  }
+  return {
+    cx,
+    cy,
+    rx,
+    ry,
+    startAngle: theta,
+    endAngle: theta + dTheta,
+    xRotation,
+    arcFlag,
+    sweepFlag,
+  };
+}
+
+export default {
+  getArcParams,
+  xAt(xRotation, rx, ry, cx, t) {
+    return rx * Math.cos(xRotation) * Math.cos(t) - ry * Math.sin(xRotation) * Math.sin(t) + cx;
+  },
+  yAt(xRotation, rx, ry, cy, t) {
+    return rx * Math.sin(xRotation) * Math.cos(t) + ry * Math.cos(xRotation) * Math.sin(t) + cy;
+  },
+  xExtrema(xRotation, rx, ry) {
+    return Math.atan((-ry / rx) * Math.tan(xRotation));
+  },
+  yExtrema(xRotation, rx, ry) {
+    return Math.atan((ry / (rx * Math.tan(xRotation))));
+  },
+};
