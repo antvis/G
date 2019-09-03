b7328556d7c5171467dee61f6c79160ae698a0f3
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-09-03 14:08:16 +0530

    udpate Webpack version & add support for bot

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const webpack = require('webpack');
+const SizePlugin = require('size-plugin');
 const resolve = require('path').resolve;
 
 module.exports = {
   entry: {
     g: './src/index.js'
   },
+  mode: process.env.NODE_ENV || 'development',
   output: {
     filename: '[name].js',
     library: 'G',
@@ -26,6 +28,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new SizePlugin({ publish: true }),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin()
   ]
