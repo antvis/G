8a1dfe1acd0cefc1d6d31c298002f01345c6f1bd
诸岳 <fuping.dfp@antfin.com>
2019-10-15 15:04:36 +0800

    site: init

---
+++ site/pages/index.zh.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SEO from '@antv/gatsby-theme-antv/site/components/seo';
+import { useTranslation } from 'react-i18next';
+
+const IndexPage = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <SEO title="蚂蚁数据可视化" lang="zh" />
+      <div style={{ margin: '0 auto', padding: '0 80px' }}>{t('首页')}</div>
+    </>
+  );
+};
+
+export default IndexPage;
