eec802ee02861867ce5f4c8ebea0855fae9b2a0b
hustcc <i@hust.cc>
2019-10-22 10:43:39 +0800

    feat: add path array type define

--- packages/g-base/src/util/path.ts
+++ packages/g-base/src/util/path.ts
@@ -1,4 +1,5 @@
 import { each, isArray } from '@antv/util';
+import { PathType } from '../types';
 
 const SPACES =
   '\x09\x0a\x0b\x0c\x0d\x20\xa0\u1680\u180e\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u202f\u205f\u3000\u2028\u2029';
@@ -9,13 +10,13 @@ const PATH_COMMAND = new RegExp(
 const PATH_VALUES = new RegExp(`(-?\\d*\\.?\\d*(?:e[\\-+]?\\d+)?)[${SPACES}]*,?[${SPACES}]*`, 'ig');
 
 // Parse given path string into an array of arrays of path segments
-const parsePathString = function(pathString) {
+const parsePathString = function(pathString: string): PathType {
   if (!pathString) {
     return null;
   }
 
-  if (typeof pathString === typeof []) {
-    return pathString;
+  if (_.isArrayLike(pathString)) {
+    return (pathString as unknown) as PathType;
   }
   const paramCounts = {
     a: 7,
