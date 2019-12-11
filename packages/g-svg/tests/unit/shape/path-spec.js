6c412372802335f196209bc1c6f30987c2374129
诸岳 <fuping.dfp@antfin.com>
2019-12-11 17:32:59 +0800

    feat(g-canvas & g-svg): add getPoint method for Path, ref #295

--- packages/g-svg/tests/unit/shape/path-spec.js
+++ packages/g-svg/tests/unit/shape/path-spec.js
@@ -5,6 +5,33 @@ import getCanvas from '../../get-canvas';
 describe('SVG path', () => {
   let canvas;
   let path;
+  const p1 = [
+    ['M', 10, 10],
+    ['L', 20, 20],
+  ];
+  const p2 = [
+    ['M', 10, 10],
+    ['Q', 20, 20, 30, 10],
+  ];
+  const p3 = [
+    ['M', 10, 10],
+    ['L', 20, 20],
+    ['C', 30, 10, 40, 30, 50, 20],
+  ];
+  const p4 = [['M', 10, 10], ['L', 20, 20], ['A', 20, 20, 0, 0, 1, 60, 20], ['Z'], ['M', 200, 200], ['L', 300, 300]];
+  const p5 =
+    'M 100,300' +
+    'l 50,-25' +
+    'a 25,25 -30 0,1 50,-25' +
+    'l 50,-25' +
+    'a 25,50 -30 0,1 50,-25' +
+    'l 50,-25' +
+    'a 25,75 -30 0,1 50,-25' +
+    'l 50,-25' +
+    'a 25,100 -30 0,1 50,-25' +
+    'l 50,-25' +
+    'l 0, 200,' +
+    'z';
 
   before(() => {
     canvas = getCanvas('svg-path');
@@ -85,21 +112,6 @@ describe('SVG path', () => {
   });
 
   it('getTotalLength', () => {
-    const p1 = [
-      ['M', 10, 10],
-      ['L', 20, 20],
-    ];
-    const p2 = [
-      ['M', 10, 10],
-      ['Q', 20, 20, 30, 10],
-    ];
-    const p3 = [
-      ['M', 10, 10],
-      ['L', 20, 20],
-      ['C', 30, 10, 40, 30, 50, 20],
-    ];
-    const p4 = [['M', 10, 10], ['L', 20, 20], ['A', 20, 20, 0, 0, 1, 60, 20], ['Z'], ['M', 200, 200], ['L', 300, 300]];
-
     path.attr('path', p1);
     expect(path.getTotalLength()).eqls(14.142135620117188);
     path.attr('path', p2);
@@ -111,22 +123,76 @@ describe('SVG path', () => {
     // Canvas 版本
     // expect(path.getTotalLength()).eqls(258.5855635430996);
     expect(path.getTotalLength()).eqls(269.3943786621094);
-    path.attr(
-      'path',
-      'M 100,300' +
-        'l 50,-25' +
-        'a 25,25 -30 0,1 50,-25' +
-        'l 50,-25' +
-        'a 25,50 -30 0,1 50,-25' +
-        'l 50,-25' +
-        'a 25,75 -30 0,1 50,-25' +
-        'l 50,-25' +
-        'a 25,100 -30 0,1 50,-25' +
-        'l 50,-25' +
-        'l 0, 200,' +
-        'z'
-    );
+    path.attr('path', p5);
     expect(path.getTotalLength()).eqls(1579.16943359375);
+  });
+
+  it.only('getPoint', () => {
+    path.attr('path', p1);
+    expect(path.getPoint(0)).eqls({
+      x: 10,
+      y: 10,
+    });
+    expect(path.getPoint(0.5)).eqls({
+      x: 15,
+      y: 15,
+    });
+    expect(path.getPoint(1)).eqls({
+      x: 20,
+      y: 20,
+    });
+    path.attr('path', p2);
+    expect(path.getPoint(0)).eqls({
+      x: 10,
+      y: 10,
+    });
+    expect(path.getPoint(0.5)).eqls({
+      x: 20,
+      y: 15.000000953674316,
+    });
+    expect(path.getPoint(1)).eqls({
+      x: 30,
+      y: 10,
+    });
+    path.attr('path', p3);
+    expect(path.getPoint(0)).eqls({
+      x: 10,
+      y: 10,
+    });
+    expect(path.getPoint(0.5)).eqls({
+      x: 28.46387481689453,
+      y: 17.35248374938965,
+    });
+    expect(path.getPoint(1)).eqls({
+      x: 50,
+      y: 20,
+    });
+    path.attr('path', p4);
+    expect(path.getPoint(0)).eqls({
+      x: 10,
+      y: 10,
+    });
+    expect(path.getPoint(0.5)).eqls({
+      x: 204.75469970703125,
+      y: 204.75469970703125,
+    });
+    expect(path.getPoint(1)).eqls({
+      x: 300,
+      y: 300,
+    });
+    path.attr('path', p5);
+    expect(path.getPoint(0)).eqls({
+      x: 100,
+      y: 300,
+    });
+    expect(path.getPoint(0.5)).eqls({
+      x: 451.3769226074219,
+      y: 33.00656509399414,
+    });
+    expect(path.getPoint(1)).eqls({
+      x: 100,
+      y: 300,
+    });
   });
 
   it('destroy', () => {
