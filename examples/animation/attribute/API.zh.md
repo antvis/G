7b2f5315c9be75f357b2e07bf32d8a2c93900f00
诸岳 <fuping.dfp@antfin.com>
2019-12-12 12:21:47 +0800

    feat(g-canvas & g-svg): add getTotalLength and getPoint method for Polyline, ref #295

--- examples/animation/attribute/API.zh.md
+++
@@ -1,8 +0,0 @@
----
-title: API
----
-
-## 属性
-
-- `container.on(eventType: string, callback: (ev: GraphEvent) => void, once: boolean)`
-  - `container`: canvas 或 group 对象
--- packages/g-svg/tests/unit/shape/polyline-spec.js
+++ packages/g-svg/tests/unit/shape/polyline-spec.js
@@ -5,12 +5,26 @@ import getCanvas from '../../get-canvas';
 describe('SVG polyline', () => {
   let canvas;
   let polyline;
+  const points1 = [
+    [10, 10],
+    [100, 10],
+    [100, 100],
+    [10, 100],
+  ];
+  const points2 = [
+    [50, 50],
+    [100, 50],
+    [100, 100],
+    [150, 100],
+    [150, 150],
+    [200, 150],
+  ];
 
   before(() => {
     canvas = getCanvas('svg-polyline');
     polyline = new Polyline({
       attrs: {
-        points: [[50, 50], [100, 100], [50, 150], [100, 200], [50, 250], [100, 300], [50, 350], [100, 400]],
+        points: points1,
         stroke: 'red',
         lineWidth: 1,
       },
@@ -19,16 +33,7 @@ describe('SVG polyline', () => {
   });
 
   it('init', () => {
-    expect(polyline.attr('points')).eql([
-      [50, 50],
-      [100, 100],
-      [50, 150],
-      [100, 200],
-      [50, 250],
-      [100, 300],
-      [50, 350],
-      [100, 400],
-    ]);
+    expect(polyline.attr('points')).eql(points1);
     expect(polyline.attr('stroke')).eql('red');
     expect(polyline.attr('lineWidth')).eql(1);
   });
@@ -40,17 +45,17 @@ describe('SVG polyline', () => {
 
   it('bbox', () => {
     const bbox = polyline.getBBox();
-    expect(bbox.minX).eql(49.5);
-    expect(bbox.minY).eql(49.5);
+    expect(bbox.minX).eql(9.5);
+    expect(bbox.minY).eql(9.5);
     expect(bbox.maxX).eql(100.5);
-    expect(bbox.maxY).eql(400.5);
+    expect(bbox.maxY).eql(100.5);
   });
 
   it('isHit', () => {
-    expect(polyline.isHit(50, 50)).eql(true);
-    expect(polyline.isHit(75, 75)).eql(true);
-    expect(polyline.isHit(75, 77)).eql(false);
-    expect(polyline.isHit(50, 100)).eql(false);
+    expect(polyline.isHit(10, 10)).eql(true);
+    expect(polyline.isHit(100, 10)).eql(true);
+    expect(polyline.isHit(100, 110)).eql(false);
+    expect(polyline.isHit(50, 50)).eql(false);
   });
 
   it('change', () => {
@@ -59,14 +64,50 @@ describe('SVG polyline', () => {
     expect(polyline.attr('lineWidth')).eql(4);
     polyline.draw(context);
     const bbox = polyline.getBBox();
-    expect(bbox.minX).eql(48);
-    expect(bbox.minY).eql(48);
+    expect(bbox.minX).eql(8);
+    expect(bbox.minY).eql(8);
     expect(bbox.maxX).eql(102);
-    expect(bbox.maxY).eql(402);
-    expect(polyline.isHit(50, 50)).eql(true);
-    expect(polyline.isHit(100, 100)).eql(true);
-    expect(polyline.isHit(75, 77)).eql(true);
-    expect(polyline.isHit(50, 100)).eql(false);
+    expect(bbox.maxY).eql(102);
+    expect(polyline.isHit(11, 11)).eql(true);
+    expect(polyline.isHit(101, 11)).eql(true);
+    expect(polyline.isHit(100, 110)).eql(false);
+    expect(polyline.isHit(50, 50)).eql(false);
+  });
+
+  it('getTotalLength', () => {
+    polyline.attr('points', points1);
+    expect(polyline.getTotalLength()).eqls(270);
+    polyline.attr('points', points2);
+    expect(polyline.getTotalLength()).eqls(250);
+  });
+
+  it('getPoint', () => {
+    polyline.attr('points', points1);
+    expect(polyline.getPoint(0)).eqls({
+      x: 10,
+      y: 10,
+    });
+    expect(polyline.getPoint(0.6)).eqls({
+      x: 100,
+      y: 82,
+    });
+    expect(polyline.getPoint(1)).eqls({
+      x: 10,
+      y: 100,
+    });
+    polyline.attr('points', points2);
+    expect(polyline.getPoint(0)).eqls({
+      x: 50,
+      y: 50,
+    });
+    expect(polyline.getPoint(0.5)).eqls({
+      x: 125,
+      y: 100,
+    });
+    expect(polyline.getPoint(1)).eqls({
+      x: 200,
+      y: 150,
+    });
   });
 
   it('destroy', () => {
