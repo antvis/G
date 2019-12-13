cbb12ddcee64232f93e067dae83e49f186614dea
诸岳 <fuping.dfp@antfin.com>
2019-12-13 10:10:14 +0800

    docs(demo): modify the import of G

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,7 @@
-window.G = require('./packages/g-canvas/src/index.ts');
-window.G_SVG = require('./packages/g-svg/src/index.ts');
+/**
+ * Note the way of mounting dependencies
+ * See: https://github.com/antvis/gatsby-theme-antv/issues/80
+ */
+window.gCanvas = require('./packages/g-canvas/src/index.ts');
+window.gSvg = require('./packages/g-svg/src/index.ts');
 window.insertCss = require('insert-css');
