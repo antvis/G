ed81df3eabafdfc43c5a1ea8d46dea6366f2cdbb
诸岳 <fuping.dfp@antfin.com>
2020-02-24 17:47:27 +0800

    fix(g-base): bbox calculation should be correct for container, close #417 and #407

--- packages/g-canvas/tests/bugs/issue-407-spec.js
+++ packages/g-canvas/tests/bugs/issue-407-spec.js
@@ -12,7 +12,7 @@ describe('#407', () => {
     height: 400,
   });
 
-  it('bbox for container should not contain Infinity and -Infinity', () => {
+  it('bbox calculation should be correct for container', () => {
     // case 1: child is a empty group
     const group1 = canvas.addGroup();
     group1.addGroup();
@@ -83,6 +83,66 @@ describe('#407', () => {
       maxY: 110,
       width: 10,
       height: 10,
+    });
+
+    // case 4: a child is empty group and another child is not
+    const group = canvas.addGroup();
+    const subGroup1 = group.addGroup();
+    const subGroup2 = group.addGroup();
+
+    subGroup2.attr('matrix', [1, 0, 0, 0, 1, 0, 100, 100, 1]);
+
+    subGroup2.addShape('rect', {
+      attrs: {
+        fill: 'red',
+        stroke: 'blue',
+        lineWidth: 1,
+        height: 100,
+        width: 150,
+        x: -75,
+        y: -50,
+      },
+    });
+
+    expect(subGroup1.getBBox()).eqls({
+      x: 0,
+      y: 0,
+      minX: 0,
+      minY: 0,
+      maxX: 0,
+      maxY: 0,
+      width: 0,
+      height: 0,
+    });
+    expect(subGroup2.getBBox()).eqls({
+      x: -75.5,
+      y: -50.5,
+      minX: -75.5,
+      minY: -50.5,
+      maxX: 75.5,
+      maxY: 50.5,
+      width: 151,
+      height: 101,
+    });
+    expect(group.getBBox()).eqls({
+      x: -75.5,
+      y: -50.5,
+      minX: -75.5,
+      minY: -50.5,
+      maxX: 75.5,
+      maxY: 50.5,
+      width: 151,
+      height: 101,
+    });
+    expect(group.getCanvasBBox()).eqls({
+      x: 24.5,
+      y: 49.5,
+      minX: 24.5,
+      minY: 49.5,
+      maxX: 175.5,
+      maxY: 150.5,
+      width: 151,
+      height: 101,
     });
   });
 });
