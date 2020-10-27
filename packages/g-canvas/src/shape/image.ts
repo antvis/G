2f0b5595c3cd67c3c2c1ac7397758cd17ac2b1dc
yuqi.pyq <yuqi.pyq@antfin.com>
2020-10-27 16:52:10 +0800

    fix: sync #639

--- packages/g-canvas/src/shape/image.ts
+++ packages/g-canvas/src/shape/image.ts
@@ -70,9 +70,9 @@ class ImageShape extends ShapeBase {
           callback.call(this);
         }
       };
-      image.src = img;
       // 设置跨域
       image.crossOrigin = 'Anonymous';
+      image.src = img;
       // loading 过程中不绘制
       this.set('loading', true);
     } else if (img instanceof Image) {
