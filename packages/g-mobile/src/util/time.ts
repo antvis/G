8cc5be021b2f4d0e1baab2fab6bdf841bdbec6fe
贤空 <wayne.wb@antfin.com>
2021-06-01 20:34:51 +0800

    feat: miniapp use native raf

--- packages/g-mobile/src/util/time.ts
+++ packages/g-mobile/src/util/time.ts
@@ -1,4 +1,14 @@
+const cache = {};
+
+export function setExtraFunction(extraData: any = {}) {
+  cache['requestAnimationFrame'] = extraData['requestAnimationFrame'];
+  cache['clearAnimationFrame'] = extraData['clearAnimationFrame'];
+}
+
 function requestAnimationFrame(fn: FrameRequestCallback) {
+  if (cache['requestAnimationFrame']) {
+    return cache['requestAnimationFrame'];
+  }
   const method =
     typeof window === 'object' && window.requestAnimationFrame
       ? window.requestAnimationFrame
@@ -9,6 +19,9 @@ function requestAnimationFrame(fn: FrameRequestCallback) {
 }
 
 function clearAnimationFrame(handler: number) {
+  if (cache['clearAnimationFrame']) {
+    return cache['clearAnimationFrame'];
+  }
   const method = typeof window === 'object' && window.cancelAnimationFrame ? window.cancelAnimationFrame : clearTimeout;
   return method(handler);
 }
