b205da70e0d34454bab206c65350d694b4cf7f2b
Susan <527971893@qq.com>
2021-08-06 08:39:48 +0800

    feat: 让fill支持传入Pattern对象 (#798)
    Co-authored-by: 酥云 <lisuwen.lsw@antgroup.com>
    Co-authored-by: Visiky <736929286@qq.com>

--- packages/g-canvas/src/util/parse.ts
+++ packages/g-canvas/src/util/parse.ts
@@ -171,11 +171,12 @@ export function parsePattern(context: CanvasRenderingContext2D, element: IElemen
   return pattern;
 }
 
-export function parseStyle(context: CanvasRenderingContext2D, element: IElement, color: string) {
+export function parseStyle(context: CanvasRenderingContext2D, element: IElement, color: string | CanvasPattern) {
   const bbox = element.getBBox();
   if (isNaN(bbox.x) || isNaN(bbox.y) || isNaN(bbox.width) || isNaN(bbox.height)) {
     return color;
   }
+
   if (isString(color)) {
     if (color[1] === '(' || color[2] === '(') {
       if (color[0] === 'l') {
@@ -191,6 +192,10 @@ export function parseStyle(context: CanvasRenderingContext2D, element: IElement,
         return parsePattern(context, element, color);
       }
     }
+    return color;
+  }
+
+  if(color instanceof CanvasPattern) {
     return color;
   }
 }
