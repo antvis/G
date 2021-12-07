453de9b1b3b5853cc50a2f2de0d6ae1e10a4a8b9
yuqi.pyq <yuqi.pyq@antgroup.com>
2021-12-07 12:37:15 +0800

    build: commit glsl_compile wasm

---
+++ rust/pkg/index.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * @param {string} source
+ * @param {string} stage
+ * @param {boolean} validation_enabled
+ * @returns {string}
+ */
+export function glsl_compile(source: string, stage: string, validation_enabled: boolean): string;
