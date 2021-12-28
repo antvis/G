368a8cfc1f12eebad2a787e86638798ea15670c6
zhanba <c5e1856@gmail.com>
2021-12-28 19:34:10 +0800

    docs: fix webgl demo

--- packages/site/examples/react-g/demo/basic.js
+++ packages/site/examples/react-g/demo/basic.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Canvas, Circle } from '@antv/react-g';
 import { Renderer as WebGLRenderer } from '@antv/g-webgl';
@@ -6,9 +6,23 @@ import { Renderer as WebGLRenderer } from '@antv/g-webgl';
 const renderer = new WebGLRenderer();
 
 const App = () => {
+  const [size, setSize] = useState(50);
   return (
-    <Canvas width={600} height={500} renderer={renderer}>
-      <Circle x={100} y={200} r={50} />
+    <Canvas width={600} height={400} renderer={renderer}>
+      <Circle
+        x={100}
+        y={200}
+        r={size}
+        fill="#1890FF"
+        stroke="#F04864"
+        lineWidth={4}
+        onMouseenter={() => {
+          setSize(100);
+        }}
+        onMouseleave={() => {
+          setSize(50);
+        }}
+      />
     </Canvas>
   );
 };
