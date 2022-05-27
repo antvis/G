03a973b868e2750360f8c9cdf016bf581b28e14f
zengyue <yezengyue@gmail.com>
2022-05-27 18:22:48 +0800

    feat: canvas 的 width/height 优先使用 style width/height

--- packages/g/src/utils/dom.ts
+++ packages/g/src/utils/dom.ts
@@ -8,3 +8,25 @@ export function setDOMSize($el: CanvasLike, width: number, height: number) {
     ($el as unknown as HTMLElement).style.height = height + PX_SUFFIX;
   }
 }
+
+export function getStyle($el: HTMLElement | CanvasLike, property: string) {
+  if (isBrowser) {
+    return document.defaultView.getComputedStyle($el as Element, null).getPropertyValue(property);
+  }
+}
+
+export function getWidth($el: HTMLElement | CanvasLike) {
+  const width = getStyle($el, 'width');
+  if (width === 'auto') {
+    return ($el as HTMLElement).offsetWidth;
+  }
+  return parseFloat(width);
+}
+
+export function getHeight($el: HTMLElement | CanvasLike) {
+  const height = getStyle($el, 'height');
+  if (height === 'auto') {
+    return ($el as HTMLElement).offsetHeight;
+  }
+  return parseFloat(height);
+}
