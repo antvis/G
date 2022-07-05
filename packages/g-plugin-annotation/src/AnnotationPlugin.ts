1f012a62290afa4350db751d24303c89d23bafd8
yuqi.pyq <yuqi.pyq@antgroup.com>
2022-07-05 09:43:09 +0800

    feat: create selectable ui

--- packages/g-plugin-annotation/src/AnnotationPlugin.ts
+++ packages/g-plugin-annotation/src/AnnotationPlugin.ts
@@ -1,11 +1,18 @@
-import type { DisplayObject, FederatedEvent, RenderingPlugin, RenderingService } from '@antv/g';
 import {
+  Canvas,
   ContextService,
+  DisplayObject,
+  FederatedEvent,
+  Group,
   inject,
   RenderingContext,
+  RenderingPlugin,
   RenderingPluginContribution,
+  RenderingService,
+  Shape,
   singleton,
 } from '@antv/g';
+import { SelectableRect } from './SelectableRect';
 import { AnnotationPluginOptions } from './tokens';
 
 /**
@@ -26,20 +33,61 @@ export class AnnotationPlugin implements RenderingPlugin {
   @inject(AnnotationPluginOptions)
   private annotationPluginOptions: AnnotationPluginOptions;
 
+  private activeSelectableLayer = new Group({
+    className: 'g-annotation-active-layer',
+    style: {
+      zIndex: 999,
+    },
+  });
+
   apply(renderingService: RenderingService) {
     const document = this.renderingContext.root.ownerDocument;
-    const canvas = document.defaultView;
+    const canvas = document.defaultView as Canvas;
+
+    const handleClick = (e: FederatedEvent) => {
+      const object = e.target as DisplayObject;
+
+      if (object.style?.selectable) {
+        const selectable = this.getOrCreateSelectableUI(object);
+
+        if (selectable) {
+          selectable.style.visibility = 'visible';
+        }
+      } else {
+        this.activeSelectableLayer.children.forEach((selectable) => {
+          selectable.style.visibility = 'hidden';
+        });
+      }
+    };
 
     renderingService.hooks.init.tapPromise(AnnotationPlugin.tag, async () => {
-      canvas.addEventListener('click', (e: FederatedEvent) => {
-        const object = e.target as DisplayObject;
-
-        if (object.style?.selectable) {
-          // 绘制辅助 UI
-        }
-      });
+      canvas.addEventListener('click', handleClick);
+      canvas.appendChild(this.activeSelectableLayer);
     });
 
-    renderingService.hooks.destroy.tap(AnnotationPlugin.tag, () => {});
+    renderingService.hooks.destroy.tap(AnnotationPlugin.tag, () => {
+      canvas.removeEventListener('click', handleClick);
+      canvas.removeChild(this.activeSelectableLayer);
+    });
+  }
+
+  private getOrCreateSelectableUI(object: DisplayObject): DisplayObject {
+    if (!object.style.selectableUI) {
+      let created: DisplayObject;
+      if (object.nodeName === Shape.IMAGE) {
+        created = new SelectableRect({
+          style: {
+            target: object,
+          },
+        });
+      }
+
+      if (created) {
+        object.style.selectableUI = created;
+        this.activeSelectableLayer.appendChild(created);
+      }
+    }
+
+    return object.style.selectableUI;
   }
 }
