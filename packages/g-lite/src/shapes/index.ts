f4fa99066578d950e28391700f4295b7321d3916
yuqi.pyq <yuqi.pyq@antgroup.com>
2022-08-28 21:20:55 +0800

    fix: animation interface

--- packages/g-lite/src/shapes/index.ts
+++ packages/g-lite/src/shapes/index.ts
@@ -1,7 +1,7 @@
 export * from './AABB';
-export * from './BoundingSphere';
+// export * from './BoundingSphere';
 export * from './Frustum';
 export * from './Plane';
 export * from './Point';
-export * from './Ray';
+// export * from './Ray';
 export * from './Rectangle';
