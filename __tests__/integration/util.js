5c14937b3db6dfff0d05e74efc99f070f06066c2
yuqi.pyq <yuqi.pyq@antgroup.com>
2023-06-08 11:49:52 +0800

    chore: use headless-gl for ssr & add to ci #1358

--- __tests__/integration/util.js
+++ __tests__/integration/util.js
@@ -13,13 +13,29 @@ const sleep = (n) => {
 /**
  * diff between PNGs
  */
-const diff = (src, target) => {
+const diff = (src, target, showMismatchedPixels = true) => {
   const img1 = PNG.sync.read(fs.readFileSync(src));
   const img2 = PNG.sync.read(fs.readFileSync(target));
   const { width, height } = img1;
-  return pixelmatch(img1.data, img2.data, null, width, height, {
+  const maxError = 10;
+
+  let diffPNG = null;
+  let output = null;
+  if (showMismatchedPixels) {
+    diffPNG = new PNG({ width, height });
+    output = diffPNG.data;
+  }
+
+  const mismatch = pixelmatch(img1.data, img2.data, output, width, height, {
     threshold: 0.1,
   });
+
+  if (showMismatchedPixels && mismatch > maxError && diffPNG) {
+    const diffPath = src.replace('.png', '.diff.png');
+    fs.writeFileSync(diffPath, PNG.sync.write(diffPNG));
+  }
+
+  return mismatch;
 };
 
 /**
@@ -31,10 +47,12 @@ const createPNGFromRawdata = async (target, width, height, data) => {
   for (let y = 0; y < newfile.height; y++) {
     for (let x = 0; x < newfile.width; x++) {
       let idx = (newfile.width * y + x) << 2;
-      newfile.data[idx] = data[idx];
-      newfile.data[idx + 1] = data[idx + 1];
-      newfile.data[idx + 2] = data[idx + 2];
-      newfile.data[idx + 3] = data[idx + 3];
+      // flipY
+      let idx2 = (newfile.width * (newfile.height - y) + x) << 2;
+      newfile.data[idx] = data[idx2];
+      newfile.data[idx + 1] = data[idx2 + 1];
+      newfile.data[idx + 2] = data[idx2 + 2];
+      newfile.data[idx + 3] = data[idx2 + 3];
     }
   }
 
