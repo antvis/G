d7308d2a93a5e13b5cbfdb82a6e4a17d9ab85568
yuqi.pyq <yuqi.pyq@antgroup.com>
2023-07-05 16:24:34 +0800

    fix: render Circle with zdog

--- packages/g-plugin-zdog-canvas-renderer/src/ZdogRendererPlugin.ts
+++ packages/g-plugin-zdog-canvas-renderer/src/ZdogRendererPlugin.ts
@@ -18,26 +18,14 @@ export class ZdogRendererPlugin implements RenderingPlugin {
       config.renderer.getConfig().enableDirtyCheck = false;
       config.renderer.getConfig().enableDirtyRectangleRendering = false;
 
-      const context = contextService.getContext();
-      // // @ts-ignore
-      // context.illo = new Illustration({
-      //   element: contextService.getDomElement() as HTMLCanvasElement,
-      //   dragRotate: false,
-      // });
-      // @ts-ignore
-      // this.illo = context.illo;
-
+      const context = contextService.getContext() as { scene: Anchor };
       this.scene = new Anchor();
-      // @ts-ignore
       context.scene = this.scene;
     });
 
     renderingService.hooks.endFrame.tap(ZdogRendererPlugin.tag, () => {
       const context = contextService.getContext() as CanvasRenderingContext2D;
       this.scene.renderGraphCanvas(context);
-
-      // @see https://zzz.dog/api#illustration-updaterendergraph
-      this.scene.updateGraph();
     });
 
     renderingService.hooks.destroy.tap(ZdogRendererPlugin.tag, () => {});
