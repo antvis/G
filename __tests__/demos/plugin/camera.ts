1c8ecd43cea0986e063e495dddd7014e2ac91b10
Aaron <yangtao.yangtao@antgroup.com>
2024-07-03 19:06:49 +0800

    fix: fix gotoLandmark duration 0 not effect (#1721)

---
+++ __tests__/demos/plugin/camera.ts
@@ -0,0 +1,26 @@
+import { Circle } from '../../../packages/g';
+
+export async function camera(context) {
+  const { canvas } = context;
+  await canvas.ready;
+
+  const circle = new Circle({
+    style: {
+      cx: 10,
+      cy: 10,
+      r: 10,
+      fill: 'red',
+    },
+  });
+
+  canvas.appendChild(circle);
+
+  const camera = canvas.getCamera();
+
+  const landmark = camera.createLandmark('camera', {
+    position: [200, 200, 500],
+    focalPoint: [200, 200, 0],
+  });
+
+  camera.gotoLandmark(landmark, { duration: 0 });
+}
