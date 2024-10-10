84f0c86645109aa440bafcb7abb925332427fd68
Aaron <yangtao.yangtao@antgroup.com>
2024-10-10 20:38:17 +0800

    chore: remove useless comments

--- packages/g-lite/src/display-objects/CustomElement.ts
+++ packages/g-lite/src/display-objects/CustomElement.ts
@@ -11,13 +11,7 @@ export interface BaseCustomElementStyleProps extends BaseStyleProps {}
 export abstract class CustomElement<
   CustomElementStyleProps,
 > extends DisplayObject<CustomElementStyleProps & BaseCustomElementStyleProps> {
-  // static get observedAttributes(): string[] {
-  //   return [];
-  // }
-
   isCustomElement = true;
-
-  // private shadowNodes: DisplayObject[] = [];
 
   constructor({
     style,
@@ -52,54 +46,4 @@ export abstract class CustomElement<
     oldParsedValue?: any,
     newParsedValue?: any,
   ): void;
-
-  // private handleMounted = (e: FederatedEvent) => {
-  //   if (e.target === this) {
-  //     // this.shadowNodes.forEach((node) => {
-  //     //   // every child and its children should turn into a shadow node
-  //     //   // a shadow node doesn't mean to be unrenderable, it's just unsearchable in scenegraph
-  //     //   node.shadow = true;
-  //     // });
-
-  //     if (this.connectedCallback) {
-  //       this.connectedCallback();
-  //     }
-  //   }
-  // };
-
-  // private handleUnmounted = (e: FederatedEvent) => {
-  //   if (e.target === this) {
-
-  //   }
-  // };
-
-  // private handleChildInserted = (e: FederatedEvent) => {
-  //   (e.target as DisplayObject).forEach((node) => {
-  //     // append children like other shapes after mounted
-  //     if (!this.isConnected) {
-  //       this.shadowNodes.push(node as DisplayObject);
-  //     }
-  //   });
-  // };
-
-  // private handleChildRemoved = (e: FederatedEvent) => {
-  //   (e.target as DisplayObject).forEach((node) => {
-  //     node.shadow = false;
-  //   });
-  // };
-
-  // private handleAttributeChanged = <Key extends keyof CustomElementStyleProps>(
-  //   e: MutationEvent,
-  // ) => {
-  //   // only listen itself
-  //   // RangeError: Maximum call stack size exceeded
-  //   if (e.target !== this) {
-  //     return;
-  //   }
-
-  //   const { attrName, prevValue, newValue } = e;
-  //   if (this.attributeChangedCallback) {
-  //     this.attributeChangedCallback(attrName as Key, prevValue, newValue);
-  //   }
-  // };
 }
