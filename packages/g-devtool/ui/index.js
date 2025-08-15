483c7d0645abd6546daf66249663ca0034d91a4d
wang1212 <mrwang1212@126.com>
2025-08-15 14:26:05 +0800

    feat: upgrade chrome extension to manifest v3 and react to v18
    - Upgrade manifest version from v2 to v3 with updated permissions format
    - Migrate background scripts to service worker
    - Update content_security_policy and web_accessible_resources format
    - Replace browser_action with action
    - Upgrade react and react-dom from v16 to v18 in g-devtool
    - Update devtool UI to support React 18 createRoot API
    - Maintain backward compatibility with legacy versions
    - Update minimum chrome version requirement to 88

--- packages/g-devtool/ui/index.js
+++ packages/g-devtool/ui/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Devtool from './components/Devtool';
 import 'antd/dist/antd.css';
 
 window.mount = (data = [], container, actions = {}) => {
-  ReactDOM.render(<Devtool data={data} actions={actions} />, container)
+  if (createRoot) {
+    // React 18+
+    const root = createRoot(container);
+    root.render(<Devtool data={data} actions={actions} />);
+  } else {
+    // 兼容旧版本
+    ReactDOM.render(<Devtool data={data} actions={actions} />, container);
+  }
 }
